11ce864155d3906194ffac8b572351f046f1785a
gustiizza <78395977+gustiizza@users.noreply.github.com>
2022-06-01 21:37:02 +0700

    Sistem Informasi Geografis Universitas Tanjungpura

---
+++ config/leafletsetup.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'zoom_level'           => 20,
+    'detail_zoom_level'    => 20,
+    'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-0.056933'),
+    'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '109.344805'),
+    // UNTUK MENGAMBIL LATITUDE DAN LONGITUDE SILAHKAN LAKUKAN SECARA MANUAL ATAU AKSES HALAMAN CREATE KEMUDIAN
+    // GESER KE LOKASI YANG DI INGINKAN
+];
